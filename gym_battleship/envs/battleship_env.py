7f84241964351ac242e852a0d306ababefded7a3
thomashirtz <37740986+thomashirtz@users.noreply.github.com>
2021-03-05 16:19:31 +0100

    Update battleship_env.py

--- gym_battleship/envs/battleship_env.py
+++ gym_battleship/envs/battleship_env.py
@@ -2,8 +2,8 @@ import gym
 import numpy as np
 from abc import ABC
 from gym import spaces
-from typing import Tuple
 from copy import deepcopy
+from typing import Tuple, Optional
 from collections import namedtuple
 
 Ship = namedtuple('Ship', ['min_x', 'max_x', 'min_y', 'max_y'])
@@ -11,17 +11,25 @@ Action = namedtuple('Action', ['x', 'y'])
 
 
 class BattleshipEnv(gym.Env, ABC):
-    def __init__(self, board_size: Tuple = None, ship_sizes: dict = None, episode_steps: int = 100):
+    def __init__(self, board_size: Tuple = None, ship_sizes: dict = None, episode_steps: int = 100, reward_dictionary: Optional[dict] = None):
         self.ship_sizes = ship_sizes or {5: 1, 4: 1, 3: 2, 2: 1}
         self.board_size = board_size or (10, 10)
 
-        self.board = None
-        self.board_generated = None
-        self.observation = None
+        self.board = None  # Hidden state updated throughout the game
+        self.board_generated = None  # Hidden state generated and left not updated (for debugging purposes)
+        self.observation = None  # the observation is a (2, n, m) matrix, the first channel is for the missile that was launched on a cell that contained a boat, the second channel is for the missile launched that was launch on a cell that was not containing any boat
 
         self.done = None
         self.step_count = None
         self.episode_steps = episode_steps
+
+        self.reward_dictionary = reward_dictionary or {  # Further tuning of the rewards required
+            'win': 100,
+            'missed': 0,
+            'touched': 1,
+            'action_already_done_missed': -0.5,
+            'action_already_done_touched': -1
+            }
 
         self.action_space = spaces.Discrete(self.board_size[0] * self.board_size[1])
         self.observation_space = spaces.MultiBinary([2, self.board_size[0], self.board_size[1]])
@@ -32,23 +40,33 @@ class BattleshipEnv(gym.Env, ABC):
 
         action = Action(x=raw_action % self.board_size[0], y=raw_action // self.board_size[0])
         self.step_count += 1
+
+        # Check if the game is done (if true, the current step is the "last step")
         if self.step_count >= self.episode_steps:
             self.done = True
 
-        if self.board[action.x, action.y] != 0:
+        # Touched action (board[x, y] == 1)
+        if self.board[action.x, action.y] == 1:
             self.board[action.x, action.y] = 0
             self.observation[0, action.x, action.y] = 1
+            # Check if there is boat(s) left
             if not self.board.any():
                 self.done = True
-                return self.observation, 100, self.done, {}
-            return self.observation, 1, self.done, {}
+                return self.observation, self.reward_dictionary['win'], self.done, {}
+            return self.observation, self.reward_dictionary['boat_touched'], self.done, {}
 
+        # Touched action already done (observation[0, x, y] == 1)
+        elif self.observation[0, action.x, action.y] == 1:
+            return self.observation, self.reward_dictionary['action_already_done_touched'], self.done, {}
+
+        # Missed action already done (observation[1, x, y] == 1)
         elif self.observation[1, action.x, action.y] == 1:
-            return self.observation, 0, self.done, {}
+            return self.observation, self.reward_dictionary['action_already_done_missed'], self.done, {}
 
+        # Action not already done and a boat was not touched
         else:
             self.observation[1, action.x, action.y] = 1
-            return self.observation, -1, self.done, {}
+            return self.observation, self.reward_dictionary['missed'], self.done, {}
 
     def reset(self):
         self.set_board()
