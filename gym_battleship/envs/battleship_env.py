66e36e50394f271cadefd6b6d456802f12f4dce3
thomashirtz <37740986+thomashirtz@users.noreply.github.com>
2021-03-13 13:45:28 +0100

    Update battleship_env.py

--- gym_battleship/envs/battleship_env.py
+++ gym_battleship/envs/battleship_env.py
@@ -11,6 +11,19 @@ Ship = namedtuple('Ship', ['min_x', 'max_x', 'min_y', 'max_y'])
 Action = namedtuple('Action', ['x', 'y'])
 
 
+def is_notebook():
+    """Helper used to change the way the environment in rendered"""
+    from IPython import get_ipython
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True  # Jupyter notebook or qtconsole
+        else:
+            return False  # Terminal running IPython or other types
+    except NameError:
+        return False  # Probably standard Python interpreter
+
+
 class BattleshipEnv(gym.Env, ABC):
     def __init__(self,
                  board_size: Tuple = None,
@@ -111,22 +124,33 @@ class BattleshipEnv(gym.Env, ABC):
         return np.count_nonzero(self.board[ship.min_x:ship.max_x, ship.min_y:ship.max_y]) == 0
 
     def render(self, mode='human'):
-        board = np.full(self.board_size, ' ', dtype=str)
+        board = np.empty(self.board_size, dtype=str)
         board[self.observation[0] != 0] = '❌'
-        board[self.observation[1] != 0] = '⚪'
+        board[self.observation[1] != 0] = '⚫'
         self._render(board)
 
     def render_board_generated(self):
-        board = np.full(self.board_size, '⬜', dtype=str)
+        board = np.empty(self.board_size, dtype=str)
         board[self.board_generated != 0] = '⬛'
         self._render(board)
 
     @staticmethod
-    def _render(board):
+    def _render(board, symbol='⬜'):
         import pandas as pd
 
         num_rows, num_columns = board.shape
         columns = [chr(i) for i in range(ord('A'), ord('A') + num_columns)]
         index = [i + 1 for i in range(num_rows)]
-        with pd.option_context('display.colheader_justify', 'right'):
-            print(pd.DataFrame(board, columns=columns, index=index), end='\n')  # todo issue IDE not align properly the df
+
+        dataframe = pd.DataFrame(board, columns=columns, index=index)
+        dataframe = dataframe.replace([''], symbol)
+
+        if is_notebook():
+            from IPython.display import display
+            display(dataframe)
+        else:
+            print(dataframe, end='\n')
+
+        # todo maybe put the board generated on the right side
+        #  https://stackoverflow.com/questions/38783027/jupyter-notebook-display-two-pandas-tables-side-by-side
+
