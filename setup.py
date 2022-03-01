78942cbbe37f2dd72f857f7508232d90b4f8e213
NiklasZ <nz911174@gmail.com>
2022-03-01 19:05:50 +0100

    Missing comma

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages, setup
 setup(
       name='gym_battleship',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'pandas']
+      install_requires=['gym', 'numpy', 'pandas'],
       packages=find_packages()
 )
