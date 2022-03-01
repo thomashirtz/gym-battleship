8d87ecdf0d51864c28cb2aba99f5015910c3644a
NiklasZ <nz911174@gmail.com>
2022-03-01 19:03:26 +0100

    Fix installation setup script

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
       name='gym_battleship',
       version='0.0.1',
       install_requires=['gym', 'numpy', 'pandas']
+      packages=find_packages()
 )
